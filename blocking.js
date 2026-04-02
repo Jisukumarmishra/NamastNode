d1ad02749d1dda3c9d0af7128966b92c828c8bf5
mishra <jisumishra4@gmail.com>
2026-04-03 03:59:47 +0530

    Learning The Concept Of Blocking The Thread

--- blocking.js
+++ blocking.js
@@ -6,8 +6,24 @@ var a = 1078698;
 var b = 20986;
 
 // pdkf2 -Password base Key Dertivativve function
-crypto.pbkdf2("password","salt", 5000000, 50, "sha512");
+//sunc:= Blocking the Thread Dont use it
+crypto.pbkdf2Sync("password","salt", 50000000, 50, "sha512");
+console.log("First Key is Generated")
+
 
 // Async function
+//Runs in background (libuv thread pool)
+crypto.pbkdf2("Passowrd", "salt", 5000000,50, "sha512", (error, key) => {
+  console.log("Second key Is Generated");
+});
 
-crypto.pbkdf2("Passowrd")
+
+function mulitplyFn(x, y) {
+  const result = a * b;
+  return result;
+}
+
+var c = mulitplyFn(a,b);
+
+console.log ("MultiPlication result is : ", c);
+
---
+++ readme.md
@@ -0,0 +1,2 @@
+ # Configure Local Repo with GitHub Repo (Steps I Often Forget)
+<!-- git remote add origin repo_link -->
